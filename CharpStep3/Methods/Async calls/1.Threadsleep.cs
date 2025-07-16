c636493c720e48ac86a4642f65b0a7f67162dc41
Pavithra354 <naralapavithra@gmail.com>
2025-07-16 21:17:15 +0530

    task

---
+++ CharpStep3/Methods/Async calls/1.Threadsleep.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadSleepNamespace
+{
+    class ThreadSleepClass
+    {
+        static void Main()
+        {
+            Console.WriteLine("Start");
+            Thread.Sleep(5000);  
+            Console.WriteLine("End");
+            Thread.Sleep(7000);
+            Console.WriteLine("Again End....");
+        }
+    }
+}
---
+++ CharpStep3/Methods/Synchronous calls.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+
+class WorkItems
+{
+
+
+    public static void PrepareWordDocumentation()
+    {
+        var startTime = DateTime.Now;
+        //Method which gives the sleep time.
+        Thread.Sleep(2000);   //
+                              //DB - 40000Records
+        var endTime = DateTime.Now;
+        var elapsedTime = endTime - startTime;
+        Console.WriteLine($"Completed the Doucumentation Elapsed time: {elapsedTime.Milliseconds} ms.");
+
+    }
+
+    public static void PreparePPTsDocumentation()
+    {
+        var startTime = DateTime.Now;
+        //Method which gives the sleep time.
+        Thread.Sleep(5000);   //
+                              //DB - 40000Records
+        var endTime = DateTime.Now;
+        var elapsedTime = endTime - startTime;
+        Console.WriteLine($"Completed the Doucumentation Elapsed time: {elapsedTime.Milliseconds} ms.");
+    }
+
+    public static void PrepareNotesDocumentation()
+    {
+        var startTime = DateTime.Now;
+        //Method which gives the sleep time.
+        Thread.Sleep(4000);   //
+                              //DB - 40000Records
+        var endTime = DateTime.Now;
+        var elapsedTime = endTime - startTime;
+        Console.WriteLine($"Completed the Doucumentation Elapsed time: {elapsedTime.Milliseconds} ms.");
+    }
+}
+
+class Trainer
+{
+    public static void Works()
+    {
+        Console.WriteLine("Sequential Works - Step By Step");
+        var preparationStartTime = DateTime.Now;
+
+        WorkItems.PrepareWordDocumentation();
+        WorkItems.PreparePPTsDocumentation();
+        WorkItems.PrepareNotesDocumentation();
+
+        var preparationEndTime = DateTime.Now;
+
+        var sequentialElapsedTime = preparationEndTime - preparationStartTime;
+
+        Console.WriteLine($"Sequential downloading completed. Total elapsed time: " +
+            $"{sequentialElapsedTime.TotalMilliseconds} ms.");
+
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        Trainer.Works();  
+    }
+
+}
